6fdd91f9568d034d17502fe4534aae4027d73f1e
Balans <gilmonpurry@hotmail.com>
2023-01-09 15:53:02 -0300

    i8n fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React , {Suspense} from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import i18n from "./i18n";
 import { I18nextProvider } from "react-i18next";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <Suspense fallback={<div>Loading...</div>}>
     <I18nextProvider i18n={i18n}>
 
      <App />
 
     </I18nextProvider>
+
+    </Suspense>
 
     
 
